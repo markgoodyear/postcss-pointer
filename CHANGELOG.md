4be3cff145866811ac5dd717c073ac145a63a417
Mark Goodyear <hello@markgoodyear.com>
2015-06-24 19:42:17 +0100

    Bump ver

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## v1.0.1
+- Code tidy.
+
 ## v1.0.0
 - Initial release.
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-pointer', function () {
-
   return function (css) {
     css.eachDecl('pointer', function (decl) {
       if (decl.value.indexOf('cursor') !== -1) {
@@ -10,5 +9,4 @@ module.exports = postcss.plugin('postcss-pointer', function () {
       }
     });
   };
-
 });
