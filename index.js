830945e4adf562b5e84e12a50df661ef14b5f0da
Bogdan Chadkin <trysound@yandex.ru>
2015-06-24 19:39:54 +0300

    Refactoring for more clear code

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ var postcss = require('postcss');
 module.exports = postcss.plugin('postcss-pointer', function () {
 
   return function (css) {
-    css.eachDecl(function transformDecl (decl) {
-      if (decl.prop === 'pointer') {
-        if (decl.value.indexOf('cursor') !== -1) {
-          decl.prop  = 'cursor';
-          decl.value = 'pointer';
-        }
+    css.eachDecl('pointer', function (decl) {
+      if (decl.value.indexOf('cursor') !== -1) {
+        decl.prop  = 'cursor';
+        decl.value = 'pointer';
       }
     });
   };
